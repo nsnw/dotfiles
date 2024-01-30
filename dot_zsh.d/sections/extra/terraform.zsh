81a136259a49a12de284014e33de93be83f41688
Andy Smith <asmith@pagerduty.com>
2024-01-30 13:18:15 -0700

    Reconcile changes

--- dot_zsh.d/sections/extra/terraform.zsh
+++ dot_zsh.d/sections/extra/terraform.zsh
@@ -4,3 +4,28 @@ alias tf='terraform'
 alias tfp='terraform plan'
 alias tfi='terraform init'
 alias tfg='terraform get'
+alias tfa='terraform apply'
+alias tff='terraform fmt'
+alias tfd='terraform apply -destroy'
+
+function _terraform_search_for_roles() {
+  GIT_ROOT="$(git rev-parse --show-toplevel)"
+
+  if [[ $? -ne 0 ]]; then
+    log_error "Not in a git repo."
+    return 1
+  fi
+
+  find $(git rev-parse --show-toplevel) -type f -name '*.tf' -maxdepth 6 \
+    | grep -v "\.terraform" \
+    | xargs -I'{}' egrep "arn:aws:iam::[0-9]+" {} \
+    | grep -v "\\$" \
+    | awk '{ print $3 }' \
+    | sort \
+    | uniq \
+    | awk -F'"' '{ print $2 }'
+}
+
+function tf-roles() {
+  _terraform_search_for_roles
+}
--- dot_zsh.d/sections/work/terraform.zsh
+++
@@ -1,6 +0,0 @@
-# Terraform
-
-alias tf='terraform'
-alias tfp='terraform plan'
-alias tfi='terraform init'
-alias tfg='terraform get'
